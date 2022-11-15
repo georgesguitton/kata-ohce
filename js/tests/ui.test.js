68a0864f6713d1104c806a338bead23bd3bff04b
Georges <georges.guitton5@gmail.com>
2022-11-15 17:33:08 +0100

    feat : ui v2

--- js/tests/ui.test.js
+++ js/tests/ui.test.js
@@ -1,8 +1,13 @@
 const test = require('tape')
 const { ohce } = require('../ohce')
+const { UI } = require('../ui')
 
 test('main loop', (t) => {
-  t.plan(1)
-  t.deepEquals(ohce(["hello", "oto", "quit"]), ["olleh", "oto", "That was a palindrome!"])
+
+  const ui = new UI({test: true})
+
+  const result = ui.mainLoop(["hello", "oto", "quit"])
+
+  t.deepEqual(result, ["olleh", "oto", "That was a palindrome!"])
   t.end()
 })
--- js/ui.js
+++ js/ui.js
@@ -2,32 +2,58 @@ const prompt = require('prompt-sync')()
 const reverse = require('./ohce').reverse
 
 class ConsoleInteractor {
-  readInput () {
+  readInput() {
     return prompt('')
   }
 
-  printMessage (message) {
+  printMessage(message) {
     console.log(message)
   }
 }
 
+class TestInteractor {
+  getInput(input) {
+    return input
+  }
+}
+
 class UI {
-  constructor () {
-    this.interactor = new ConsoleInteractor()
+  constructor ({ test = false } = {}) {
+    if (test) {
+      this.interactor = new TestInteractor()
+    } else {
+      this.interactor = new ConsoleInteractor()
+    }
   }
 
-  mainLoop () {
+  mainLoop (inputs = undefined) {
+    let count = 0
+    let result = []
     while (true) {
-      const input = this.interactor.readInput()
+      let input = ""
+      if (inputs === undefined) {
+        input = this.interactor.readInput()
+      } else {
+        input = this.interactor.getInput(inputs[count])
+      }
       if (input === 'quit') {
         break
       }
       const reversed = reverse(input)
-      this.interactor.printMessage(reversed)
-      if (input === reversed) {
-        this.interactor.printMessage('That was a palindrome!')
+      result.push(reversed)
+      count++
+      if (inputs === undefined) {
+        this.interactor.printMessage(reversed)
+        if (input === reversed) {
+          this.interactor.printMessage('That was a palindrome!')
+        }
+      } else {
+        if (input === reversed) {
+          result.push("That was a palindrome!")
+        }
       }
     }
+    return result
   }
 }
 
